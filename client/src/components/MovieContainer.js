6cdf8e4b3fcf47160f44e594762e4fc996324878
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-02-16 20:39:02 +1100

    added backend files

---
+++ client/src/components/MovieContainer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import MovieForm from "./ MovieForm";
+import List from "./List";
+class MovieContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      moviesList: [],
+    };
+  }
+
+  handleMovieFormSubmit = (title, genre, description) => {
+    console.log("formValues:", title, genre);
+    console.log("this:", this);
+    // Read title and genre state and put in a temp variable which is Obj literal
+    const newMovie = { genre: genre, title: title, description: description };
+    // Create a new movies array variable which is a copy from movies state via ... operator
+    const newMovies = [...this.state.moviesList];
+    newMovies.push(newMovie);
+    // Set the state for movies and pass the new movies array.
+    this.setState({ moviesList: newMovies });
+
+    fetch('http://localhost:9000/api/v1/movies', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newMovie)
+    }).then((response) => {
+      console.log("response", response);
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Movies</h1>
+        <List movies={this.state.moviesList} />
+        <MovieForm submit={this.handleMovieFormSubmit} />
+      </div>
+    );
+  }
+}
+
+export default MovieContainer;
--- movies-json-api/models/movie.js
+++ movies-json-api/models/movie.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
   title: String,
   description: String,
+  genre: String,
 });
 
 
